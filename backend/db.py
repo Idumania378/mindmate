b609eccf228bac1897bf4d1c1db95fa3a420c60c
Edwin <94903048+Idumania378@users.noreply.github.com>
2025-07-18 11:53:29 +0300

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
 import os
 
 db = SQLAlchemy()
 
 def init_mysql(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = (
-        f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}/{os.environ['DB_NAME']}"
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    return db
