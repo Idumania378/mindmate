e12afcf5657b97f39ca0c31e3a8bb72ba2d37cff
Edwin <94903048+Idumania378@users.noreply.github.com>
2025-07-18 14:37:47 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -39,8 +39,6 @@ def test_db():
         return {"error": str(e)}, 500
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
 
 
