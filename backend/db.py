e331163615791267f92073bcb35dc1222d2d4bae
Edwin <94903048+Idumania378@users.noreply.github.com>
2025-07-18 12:01:20 +0300

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-import os
 
+# Create a single SQLAlchemy instance to be used across the app
 db = SQLAlchemy()
 
-def init_mysql(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-    )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
